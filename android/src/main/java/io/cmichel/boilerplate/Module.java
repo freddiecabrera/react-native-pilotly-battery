1699a8bda59802b4069eb37863ace7158bde7466
freddiecabrera <freddiecabrera18@yahoo.com>
2017-05-08 13:37:05 -0700

    f

--- android/src/main/java/io/cmichel/boilerplate/Module.java
+++ android/src/main/java/io/cmichel/boilerplate/Module.java
@@ -5,6 +5,13 @@ import android.widget.Toast;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import android.content.BroadcastReceiver;
+import android.os.BatteryManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,4 +42,27 @@ public class Module extends ReactContextBaseJavaModule {
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
+
+  @ReactMethod
+  public void isCharging(Callback successCallback) {
+    // int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+
+
+
+    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+    Intent batteryStatus = getReactApplicationContext().registerReceiver(null, ifilter);
+
+    // // Are we charging / charged?
+    // int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+    // boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+    //                      status == BatteryManager.BATTERY_STATUS_FULL;
+
+    // How are we charging?
+    int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+    //boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+    //boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+    successCallback.invoke(chargePlug == BatteryManager.BATTERY_PLUGGED_USB);
+  }
+
 }
